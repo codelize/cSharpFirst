20ed3cbce01d99e91cf946fbffa8530933128776
leonardo.lizier <leonardo.lizier@BRLJ20G344>
2025-04-10 20:54:31 -0300

    Adicionar arquivos de projeto.

---
+++ tetse/Program.cs
@@ -0,0 +1,11 @@
+﻿byte minhaIdade = 27;
+double meuPeso = 84.5;
+double minhaAltura = 1.80;
+
+// Exibindo as informações do console
+Console.WriteLine("Esses são seus dados:");
+Console.WriteLine($"Você tem {minhaIdade} anos.");
+Console.WriteLine($"Seu peso é {meuPeso} kilos.");
+Console.WriteLine($"Sua altura é {minhaAltura} .");
+
+Console.ReadLine();
